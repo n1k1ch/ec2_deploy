a508b6822380eceb08c9c8e878b35ceae23e62bd
Nikita Cherevkov <ncherevkov@csod.com>
2018-03-02 16:59:12 +0200

    initial commit

---
+++ src/Nikich.TennisStats.Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nikich.TennisStats.Web.Controllers
+{
+    [Route("api")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello";
+        }
+
+        [HttpGet("ping")]
+        public ActionResult Get(int id)
+        {
+            return Ok();
+        }
+    }
+}
